5255d12509ebf5a07d2e9cd30b6d94ef1e3ae654
Yazil Ansari <yazilansari03@gmail.com>
2023-05-04 13:10:51 +0530

    Added fetchCities Route

--- application/controllers/Services.php
+++ application/controllers/Services.php
@@ -12,69 +12,56 @@ class Services extends CI_Controller {
 			if($q->num_rows() > 0) {
 				$otp = rand(100000, 999999);
 				$message = $otp.' is your Onespace Verification Code. Enjoy Making Your Dream Space With Onespace';
-		        if (strlen($mobile_no) > 10) {
-		            $mob_no = str_replace("+91", "", $mobile_no);
-		            $mob_no = ltrim($mob_no, "0");
-		            if (strlen($mob_no) > 10) {
-		                $mob_no = ltrim($mob_no, "91");
-		            }
-		        } else if (strlen($mobile_no) == 10) {
-		            $mob_no = $mobile_no;
-		        } else {
-		            $mob_no = '';
-		        }
-		        if ($mob_no) {
-		            $apiKey = urlencode('6348eda2cf8cf');
-		            // Message details
-		            $numbers = array($mobile_no);
-		            $sender = urlencode('ONESPC');
-		            $message = rawurlencode($message);
+	            $apiKey = urlencode('6348eda2cf8cf');
+	            // Message details
+	            $numbers = array($mobile_no);
+	            $sender = urlencode('ONESPC');
+	            $message = rawurlencode($message);
 
-		            $numbers = implode(',', $numbers);
+	            $numbers = implode(',', $numbers);
 
-		            // Prepare data for POST request
-		            $data = array('apikey' => $apiKey, 'mobileno' => $numbers, 'sender' => $sender, 'text' => $message);
+	            // Prepare data for POST request
+	            $data = array('apikey' => $apiKey, 'mobileno' => $numbers, 'sender' => $sender, 'text' => $message);
 
-		            $curl = curl_init();
+	            $curl = curl_init();
 
-		            curl_setopt_array($curl, array(
-		                CURLOPT_URL => 'http://sms.mobileadz.in/api/push.json?apikey='.$apiKey.'&sender='.$sender.'&mobileno='.$numbers.'&text='.$message,
-		                CURLOPT_RETURNTRANSFER => true,
-		                CURLOPT_ENCODING => '',
-		                CURLOPT_MAXREDIRS => 10,
-		                CURLOPT_TIMEOUT => 0,
-		                CURLOPT_FOLLOWLOCATION => true,
-		                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-		                CURLOPT_CUSTOMREQUEST => 'GET',
-		            ));
+	            curl_setopt_array($curl, array(
+	                CURLOPT_URL => 'http://sms.mobileadz.in/api/push.json?apikey='.$apiKey.'&sender='.$sender.'&mobileno='.$numbers.'&text='.$message,
+	                CURLOPT_RETURNTRANSFER => true,
+	                CURLOPT_ENCODING => '',
+	                CURLOPT_MAXREDIRS => 10,
+	                CURLOPT_TIMEOUT => 0,
+	                CURLOPT_FOLLOWLOCATION => true,
+	                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	                CURLOPT_CUSTOMREQUEST => 'GET',
+	            ));
 
-		            $curl_response = curl_exec($curl);
-		            $decoded_res = json_decode($curl_response);
-		            // echo "<pre>";print_r($decoded_res);die();
-		            if($decoded_res && $decoded_res->status == 'success') {
+	            $curl_response = curl_exec($curl);
+	            $decoded_res = json_decode($curl_response);
+	            // echo "<pre>";print_r($decoded_res);die();
+	            if($decoded_res && $decoded_res->status == 'success') {
 
-		            	$this->db->where('telephone', $mobile_no)->update('os_customer', array('otp' => $otp));
+	            	$this->db->where('telephone', $mobile_no)->update('os_customer', array('otp' => $otp));
 
-		            	$response['success'] = 1;
-						$response['message'] = 'Your OTP has been Sent.';
-						header('Content-Type: application/json; charset=utf-8');
-						echo json_encode($response);
-		            } else {
-		            	$response['success'] = 0;
-						$response['message'] = $decoded_res->description;
-						header('Content-Type: application/json; charset=utf-8');
-						echo json_encode($response);
-		            }
-		            curl_close($curl);
-		        }
+	            	$response['success'] = true;
+					$response['message'] = 'Your OTP has been Sent.';
+					header('Content-Type: application/json; charset=utf-8');
+					echo json_encode($response);
+	            } else {
+	            	$response['success'] = false;
+					$response['message'] = 'Error Occurred While Sending OTP.';
+					header('Content-Type: application/json; charset=utf-8');
+					echo json_encode($response);
+	            }
+		        curl_close($curl);
 			} else {
-				$response['success'] = 0;
+				$response['success'] = false;
 				$response['message'] = 'Your Mobile Number is Not Registered.';
 				header('Content-Type: application/json; charset=utf-8');
 				echo json_encode($response);
 			}
 		} else {
-			$response['success'] = 0;
+			$response['success'] = false;
 			$response['message'] = 'Please Enter Mobile Number.';
 			header('Content-Type: application/json; charset=utf-8');
 			echo json_encode($response);
@@ -86,13 +73,13 @@ class Services extends CI_Controller {
 		$mobile_no = $this->input->post('mobile_no');
 		$otp = $this->input->post('otp');
 		if(empty($mobile_no)) {
-			$response['success'] = 0;
+			$response['success'] = false;
 			$response['message'] = 'Please Enter Mobile Number.';
 			header('Content-Type: application/json; charset=utf-8');
 			echo json_encode($response);exit();
 		}
 		if(empty($otp)) {
-			$response['success'] = 0;
+			$response['success'] = false;
 			$response['message'] = 'Please Enter OTP.';
 			header('Content-Type: application/json; charset=utf-8');
 			echo json_encode($response);exit();
@@ -100,13 +87,36 @@ class Services extends CI_Controller {
 		$q = $this->db->select('customer_id, firstname, lastname, email, telephone')->where(array('telephone' => $mobile_no, 'otp' => $otp))->get('os_customer');
 		$response = array();
 		if($q->num_rows() > 0) {
-			$response['success'] = 1;
-			$response['data'] = $q->row();
+
+			$this->db->where('telephone', $mobile_no)->update('os_customer', array('otp' => 0));
+
+			$response['success'] = true;
+			$response['message'] = 'Logged in Successfully.';
+			$response['response'] = $q->row();
 			header('Content-Type: application/json; charset=utf-8');
 			echo json_encode($response);
 		} else {
-			$response['success'] = 0;
+			$response['success'] = false;
 			$response['message'] = 'Invalid OTP.';
+			header('Content-Type: application/json; charset=utf-8');
+			echo json_encode($response);
+		}
+	}
+
+	public function fetch_cities()
+	{
+		$q = $this->db->select('id, city')->where('status', 1)->get('os_signup_location');
+		$response = array();
+		if($q->num_rows() > 0) {
+
+			$response['success'] = true;
+			$response['message'] = 'Fetched Successfully.';
+			$response['response'] = $q->result();
+			header('Content-Type: application/json; charset=utf-8');
+			echo json_encode($response);
+		} else {
+			$response['success'] = false;
+			$response['message'] = 'No City Found.';
 			header('Content-Type: application/json; charset=utf-8');
 			echo json_encode($response);
 		}
